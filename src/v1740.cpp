0f1f84a2f332699e38483cfa7ee75df9a43e771e
b.gao <gaobsh@impcas.ac.cn>
2019-06-04 23:41:56 +0800

    check the run status of v1740 before turnint it on

--- src/v1740.cpp
+++ src/v1740.cpp
@@ -107,6 +107,10 @@ int v1740::on_start()
 	RET_IF_NONZERO(ret);
 
 	/* start acquasition */
+	do {
+		ret = read_reg(0x8104, 32, &dum);
+		RET_IF_NONZERO(ret);
+	} while (!(dum & 0x100))
 	ret = read_reg(0x8100, 32, &dum);
 	RET_IF_NONZERO(ret);
 	dum |= 0x4;
