3966954465e08375b7380ee5c6edc1b5d6a54ec2
undaq <undaq@localhost.localdomain>
2020-02-17 12:34:54 -0500

    Added the start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-cd python/control
-xterm -hold -e ./control.py&
-
-cd ../..
-sleep 2
-xterm -hold -e ./frontend &
-sleep 0.5
-xterm -hold -e ./event_builder &
-sleep 0.5
-xterm -hold -e ./logger &
-sleep 0.5
-xterm -hold -e ./analyzer &
+pkill frontend
+pkill event_builder
+pkill logger
+pkill analyzer
+ ./frontend &
+ ./event_builder &
+ ./logger &
+ ./analyzer &
 
